b9d14d6804bf920ac92f99237f8f0eef40df7ab3
Collin Le <collin_le@outlook.com>
2026-09-09 17:05:11 -0400

    Add files via upload
    TerminalMsgJava folder is the actual executable files. MsgJava folder is the project folder

---
+++ MsgJava/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+        String name = input.nextLine();
+        System.out.println("Enter IP");
+        String IP = input.nextLine();
+        System.out.println("Enter a port number (12345)");
+        int port = input.nextInt();
+
+
+
+        Peer peer1 = new Peer(name, IP, port);
+
+        peer1.startService();
+    }
+}
+    
